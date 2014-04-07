235592458bb2a446e673614fed7629d4b08120ed
cogo79 <migobigo76@gmail.com>
2014-04-07 17:53:08 +0200

    Now cards can be turned face up and face down.

--- MVC/CardView.js
+++ MVC/CardView.js
@@ -4,6 +4,7 @@ var cardHeight = 84;
 var CardView = function(x, y, stage) {
 	var positionX = x;
 	var positionY = y;
+	this.cardIsFaceDown = false;
 
 	cardMask = new PIXI.Graphics();
 	cardMask.beginFill();
@@ -28,6 +29,22 @@ var CardView = function(x, y, stage) {
 		console.log("cardSprite.x", cardSprite.x);
 		console.log("cardSprite.y", cardSprite.y);
 	}
+	var coloredRectangle = new PIXI.Graphics();
+	coloredRectangle.beginFill(0x1E90FF);
+	coloredRectangle.drawRect(9, 9, 42, 66);
+	coloredRectangle.endFill();
+	this.faceDown = function() {
+		if (!this.cardIsFaceDown) {
+			cardSprite.addChild(coloredRectangle);
+			this.cardIsFaceDown = true;
+		}
+	};
+	this.faceUp = function() {
+		if (this.cardIsFaceDown) {
+			cardSprite.removeChild(coloredRectangle);
+			this.cardIsFaceDown = false;
+		}
+	};
 
 	return this;
 };
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Blackjack
 =========
 
 Just a simple Blackjack game
+
+You nead to got to www.pixijs.com and install PIXI at the same place as this readme file.
