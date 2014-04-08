4b7bab083b60dafb6f154c8612a44211bedea32c
cogo79 <migobigo76@gmail.com>
2014-04-08 16:38:51 +0200

    I just rearranged some files.

---
+++ MVC/Controller/Controller.js
@@ -0,0 +1,12 @@
+var Controller = function() {
+	return this;
+}
+
+Controller.prototype.newGame = function() {
+	
+	var deckOfCards = new DeckOfCards();
+	var dealer = new Dealer();
+	var player = new Player();
+	
+	
+}
---
+++ MVC/View/CardView.js
@@ -0,0 +1,104 @@
+var cardWidth = 60;
+var cardHeight = 84;
+
+var CardView = function(x, y, stage) {
+	var positionX = x;
+	var positionY = y;
+	var cardText = "2d";
+	var isFaceDownLocalBoolean = false;
+
+	cardMask = new PIXI.Graphics();
+	cardMask.beginFill();
+	cardMask.drawRect(x, y, cardWidth, cardHeight);
+	cardMask.endFill();
+
+	stage.addChild(cardMask);
+
+	var cardSprite = new PIXI.Sprite.fromImage("cards.png");
+	cardSprite.x = x;
+	cardSprite.y = y;
+
+	stage.addChild(cardSprite);
+
+	cardSprite.mask = cardMask;
+
+	this.setCard = function(text) {
+		cardSprite.x = positionX - cardWidth * getCardDenomination(text);
+		cardSprite.y = positionY - cardHeight * getCardSuite(text);
+		cardText = text;
+	}
+	var coloredRectangle = new PIXI.Graphics();
+	coloredRectangle.beginFill(0x1E90FF);
+	coloredRectangle.drawRect(9, 9, 42, 66);
+	coloredRectangle.endFill();
+
+	this.turnFaceDown = function() {
+		var temp = cardText;
+		this.setCard("2s");
+		cardText = temp;
+		cardSprite.addChild(coloredRectangle);
+		isFaceDownLocalBoolean = true;
+	};
+	this.turnFaceUp = function() {
+		cardSprite.removeChild(coloredRectangle);
+		this.setCard(cardText);
+		isFaceDownLocalBoolean = false;
+	};
+	
+	this.isFaceDown = function() {
+		return isFaceDownLocalBoolean;
+	}
+	
+	return this;
+};
+
+var getCardSuite = function(text) {
+		var lastChar = text.charAt(text.length - 1);
+		switch(lastChar) {
+			case "s":
+				return 0;
+				break;
+
+			case "h":
+				return 1;
+				break;
+
+			case "c":
+				return 2;
+				break;
+
+			case "d":
+				return 3;
+				break;
+
+			default:
+
+		}
+	}
+	var getCardDenomination = function(text) {
+		var firstChar = text.charAt(0);
+		switch(firstChar) {
+			case "A":
+				return 12;
+				break;
+
+			case "K":
+				return 11;
+				break;
+
+			case "Q":
+				return 10;
+				break;
+
+			case "J":
+				return 9;
+				break;
+
+			case "1":
+				return 8;
+				break;
+
+			default:
+				return parseInt(firstChar)-2;
+		}
+	}
